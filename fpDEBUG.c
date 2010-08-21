2856afb36d4641620266c126eef275a73e952f55
Henry Gebhardt <hsggebhardt@googlemail.com>
2010-08-21 02:29:18 +0200

    fpDEBUG.c: Always print to stderr

--- fpDEBUG.c
+++ fpDEBUG.c
@@ -117,9 +117,9 @@ int debugging_msg_(char const * const msg, ...)
   va_copy(ap, ag);
 
   if (va_arg(ap, char *))
-    retval = vfprintf(stdout, msg, ag);
+    retval = vfprintf(stderr, msg, ag);
   else
-    retval = vfprintf(stdout, msg + strlen("%s:"), ap);
+    retval = vfprintf(stderr, msg + strlen("%s:"), ap);
 
   va_end(ap);
   va_end(ag);
